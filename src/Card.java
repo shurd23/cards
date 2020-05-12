aa8da2ef2cbaba190dba3377fdf8a2abd4e1c3c6
shurd23 <63660086+shurd23@users.noreply.github.com>
2020-05-11 21:30:10 -0600

    Update Card.java

--- src/Card.java
+++ src/Card.java
@@ -1,17 +1,11 @@
-public class Card {
-    String suit;
-    String rank;
-    int Denomonation;
-    Suit aSuit;
-    Rank aRank;
-    public Card(String Ranks , String Suits, int rankDenomination ){
-        rank=rankDenomination;
-    suit=Suits;
-    Denomonation= rankDenomination;
-    aSuit = new Suit(suit,0);
-    aRank = new Rank(rank,Denomination);
-}
+public class Card
+{
+    private Rank Ranks;
+    private Suit Suits;
 
-    public String card(aRank + " of "+ aSuit.toString);
-
+    public Card(Rank Ranks, Suit Suits)
+    {
+        Ranks.getValue();
+        Suits.getValue();
     }
+}
